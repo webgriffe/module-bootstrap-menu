40c9a9dae7b3bedf05365df1627f120373f4cb6b
Gary Christian <gchristian@tti-fc.com>
2020-12-01 16:30:45 +0000

    update component name

--- registration.php
+++ registration.php
@@ -2,6 +2,6 @@
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::MODULE,
-    'Webgriffe_BootstrapMenu',
+    'VaxLtd_BootstrapMenu',
     __DIR__
 );
